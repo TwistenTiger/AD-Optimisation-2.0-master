861cc75a4bf84b67becc4949195d320cfd56cdbb
TwistenTiger <91313465+TwistenTiger@users.noreply.github.com>
2021-11-10 21:31:18 +0800

    Initial commit

---
+++ AD-Optimisation-2.0-master/cmake-build-debug/CMakeFiles/AD_Optimisation.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "AD_Optimisation.exe"
+  "AD_Optimisation.exe.manifest"
+  "AD_Optimisation.pdb"
+  "CMakeFiles/AD_Optimisation.dir/main.cpp.obj"
+  "libAD_Optimisation.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AD_Optimisation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
